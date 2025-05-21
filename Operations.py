d7d7887a4b4317f3fcde6241c2f05d5c9d66ba9d
Sergio CM <sergiocmdev@gmail.com>
2025-05-21 13:51:31 +0200

    deleted double method

--- Operations.py
+++ Operations.py
@@ -9,9 +9,6 @@ def subs(val1, val2):
         return 0
     return val1 - val2
 
-def product(val1, val2):
-    return val1 * val2
-
 def division(val1, val2):
     if val1 == 0 or val2 == 0:
         return 0
